4c3211c8059c016c6fa11d857374fae62a901594
Michelle Hoelzer <hoelzermm@gmail.com>
2018-08-20 11:44:08 -0400

    first

---
+++ main.js
@@ -0,0 +1,15 @@
+// onclick = drop red or balck
+//     win condition
+//         alert if one wins
+//     if not return
+
+const currentPlayer = "red";
+const nextPlayer = "black";
+
+const playerRedSelections = new Array();
+const playerBlackSelections = new Array();
+
+let testing = document.getElementById("column7")
+testing.onclick = function() {
+    alert("testing")
+}
